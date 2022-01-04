58926212ad94890a7f9882cf790d334abb770afd
David McGinnis <dmcginnis427@gmail.com>
2022-01-04 09:49:45 +0100

    pcb design seems finished

--- code/cubeCode/cubeCode.ino
+++ code/cubeCode/cubeCode.ino
@@ -1,20 +1,19 @@
-#define DEV_ADDR 101
-#define RF_FREQ  433.200
-/*
- * idata[0] = trip1
- * idata[1] = trip2
- * idata[2] = temp raw
- * idata[3] = current offset raw * 32
- * idata[4] = current stdev raw  * 32
- * idata[5] = current rmsMax * 32
- * idata[6] = current rmsAvg over int  * 32
- */
+#define DEV_ADDR   103
+#define RF_FREQ    433.200
+#define RFM95_CS   8
+#define RFM95_RST  4
+#define RFM95_INT  3
+#define RREF       4300.0
+#define RNOMINAL   1000.0
+#define commLEDPin 13
+#define trip1Pin 5
+#define trip2Pin 6
+#define reedPin 18
+
+#include "Adafruit_MAX31865.h"
+
 #include <SPI.h>
 #include "RH_RF95.h"
-
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 3
 
 RH_RF95::ModemConfigChoice modeConfig[] = {
       RH_RF95::ModemConfigChoice::Bw125Cr45Sf128, 
@@ -24,36 +23,18 @@ RH_RF95::ModemConfigChoice modeConfig[] = {
  
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
+
+
 int sigPower = 20;
 int modemConfigIndex = 0;
 float rfFreq = RF_FREQ;
-
-const int currentMonPin = A0;
-const int vbatPin = A7;
-const int commLEDPin = 13;
-const int trip1Pin = 11;
-const int trip2Pin = 12;
-const int tempPin = A1;
-
-float avgAdc = 0.0;
-float nsamples = 5000.0;
-float nsamplesCnt = 1.0;
-float avgRms = 0.0;
-float adcValue;
-float rmsAdc;
-float nfilter = 5.0;
-float filteredAdc = 0.0;
-float nfilterCnt = 1.0;
-int icnt = 0;
-float rmsMax = 0.0;
-float rmsSum = 0.0;
-float rmsCnt = 0.0;
 struct RadioPacketSend
 {
   uint16_t iaddr = DEV_ADDR;
   uint16_t irssi = 0;
   uint16_t ideltat = 0;
-  uint16_t idata[11] = {0,0,0,0,0,0,0,0,0,0,0};
+  uint16_t idata[11] = {0,0,0,0,0,0,0,0,0,0,1};
 };
 RadioPacketSend radioPacketSend;
 uint8_t sizeOfRadioPacketSend = sizeof(radioPacketSend);
@@ -71,6 +52,8 @@ unsigned long tsent;
 
 void setup() 
 {
+  Serial.begin(9600);
+
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
   delay(100);
@@ -82,46 +65,24 @@ void setup()
   rf95.init();
   rf95.setFrequency(rfFreq);
   rf95.setModemConfig(modeConfig[modemConfigIndex]); 
-
   rf95.setTxPower(sigPower, false);
 
-  pinMode(currentMonPin, INPUT);
-  pinMode(vbatPin, INPUT);
+  thermo.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary  
+ 
   pinMode(commLEDPin, OUTPUT);
   pinMode(trip1Pin, INPUT);
   pinMode(trip2Pin, INPUT);
-  pinMode(tempPin, INPUT);
-  digitalWrite(commLEDPin, LOW);
+  pinMode(reedPin, INPUT);
 
+  digitalWrite(commLEDPin, LOW);
   tnow  = millis();
   tsent = tnow;
-//  Serial.begin(9600);
-//  delay(5000);
-  
+  delay(1000);
 }
+
+
 void loop() 
 {
-  float frms = 0.0;
-  radioPacketSend.idata[0] = (uint16_t) digitalRead(trip1Pin);
-  radioPacketSend.idata[1] = (uint16_t) digitalRead(trip2Pin);
-  radioPacketSend.idata[2] = (uint16_t) analogRead(tempPin);
-  
-  adcValue = (float) analogRead(currentMonPin);
-  filteredAdc = filteredAdc + (adcValue - filteredAdc) / nfilterCnt;
-  avgAdc = avgAdc + (adcValue - avgAdc) / nsamplesCnt;
-  rmsAdc = (filteredAdc - avgAdc);
-  rmsAdc = rmsAdc * rmsAdc;
-  avgRms = avgRms + (rmsAdc - avgRms) / nsamplesCnt;
-  if (nsamplesCnt < nsamples) nsamplesCnt = nsamplesCnt + 1.0;
-  if (nfilterCnt < nfilter) nfilterCnt = nfilterCnt + 1.0;
- 
-  frms = 32.0 * sqrt(avgRms);
-  if (rmsMax < frms ) rmsMax = frms;
-  rmsSum = rmsSum + frms;
-  rmsCnt = rmsCnt + 1;
-  
-  radioPacketSend.idata[3] = (uint16_t) (32.0 * avgAdc);
-  radioPacketSend.idata[4] = (uint16_t) frms;
 
   while (rf95.available())
   {
@@ -130,16 +91,23 @@ void loop()
       if (radioPacketRecv.iaddr == radioPacketSend.iaddr)
       {
         
+        radioPacketSend.idata[0] = (uint16_t) digitalRead(trip1Pin);
+        radioPacketSend.idata[1] = (uint16_t) digitalRead(trip2Pin);
+        radioPacketSend.idata[2] = readTemperature();
+        radioPacketSend.idata[3] = (uint16_t) 16384;
+        radioPacketSend.idata[4] = 10;
+        radioPacketSend.idata[7] = (uint16_t) digitalRead(reedPin);
+        for (int ii = 0; ii < 10; ++ii)
+        {
+          Serial.print(radioPacketSend.idata[ii]);
+          Serial.print(",  ");
+        }
+        Serial.println(radioPacketSend.idata[10]);
+        
         radioPacketSend.irssi = (uint16_t) rf95.lastRssi();
         tnow  = millis();
         radioPacketSend.ideltat = (uint16_t) ((tnow - tsent) / 100); // 0.1 secs
         tsent = tnow;
-        if (rmsCnt > 1.0) rmsSum = rmsSum / rmsCnt;
-        radioPacketSend.idata[5] = (uint16_t) rmsMax;
-        radioPacketSend.idata[6] = (uint16_t) rmsSum;
-        rmsMax = 0.0;
-        rmsSum = 0.0;
-        rmsCnt = 0.0;
         
         delay(200);
         digitalWrite(commLEDPin, HIGH);
@@ -151,4 +119,30 @@ void loop()
       }
     }
   }
+
+//  uint16_t temperature = readTemperature();
+//  Serial.print("Temperature = "); Serial.println(temperature);
+//  delay(1000);
+}
+
+uint16_t readTemperature()
+{
+  uint16_t rtd = thermo.readRTD();
+  float temperature  = thermo.temperature(RNOMINAL, RREF);
+
+  uint8_t fault = thermo.readFault();
+  if (fault) 
+  {
+//    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+//    if (fault & MAX31865_FAULT_HIGHTHRESH) Serial.println("RTD High Threshold"); 
+//    if (fault & MAX31865_FAULT_LOWTHRESH)  Serial.println("RTD Low Threshold"); 
+//    if (fault & MAX31865_FAULT_REFINLOW)   Serial.println("REFIN- > 0.85 x Bias"); 
+//    if (fault & MAX31865_FAULT_REFINHIGH)  Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+//    if (fault & MAX31865_FAULT_RTDINLOW)   Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+//    if (fault & MAX31865_FAULT_OVUV)       Serial.println("Under/Over voltage"); 
+    thermo.clearFault();
+  }
+  temperature = round((temperature + 273.15) * 100);
+  uint16_t utemperature = (uint16_t) temperature;
+  return utemperature;
 }
