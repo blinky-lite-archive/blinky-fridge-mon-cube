49eb9a5535cf42d2a502df109a449c464a02a294
David McGinnis <dmcginnis427@gmail.com>
2022-01-04 10:14:07 +0100

    updated cube code for current meas

--- code/cubeCode/cubeCode.ino
+++ code/cubeCode/cubeCode.ino
@@ -172,12 +172,6 @@ uint16_t readTemperature()
   if (fault) 
   {
 //    Serial.print("Fault 0x"); Serial.println(fault, HEX);
-//    if (fault & MAX31865_FAULT_HIGHTHRESH) Serial.println("RTD High Threshold"); 
-//    if (fault & MAX31865_FAULT_LOWTHRESH)  Serial.println("RTD Low Threshold"); 
-//    if (fault & MAX31865_FAULT_REFINLOW)   Serial.println("REFIN- > 0.85 x Bias"); 
-//    if (fault & MAX31865_FAULT_REFINHIGH)  Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
-//    if (fault & MAX31865_FAULT_RTDINLOW)   Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
-//    if (fault & MAX31865_FAULT_OVUV)       Serial.println("Under/Over voltage"); 
     thermo.clearFault();
   }
   temperature = round((temperature + 273.15) * 100);
